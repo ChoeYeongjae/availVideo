1c6a6630f2b5a397a472a9ecf7212dbb6361c563
nhd5913 <nhd5913@naver.com>
2020-01-16 22:13:35 +0900

    비디오, 회원목록 저장 기능 개발

--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -11,74 +11,71 @@ import java.util.ArrayList;
 
 public class main {
 
-	public static void memberListTrans(String member_list) {
-
-		// 멤버 객체 
-		ArrayList<Member> members = new ArrayList<Member>();
+	// 회원목록 저장  
+	public static void member_save(String files) {
 		
+		// MemberManger에 담을 객체생성함.
+		MemberManger  membermanger = new MemberManger(); 
+			
 		try{
-            //파일 객체 생성
-	        File file = new File(member_list);
-	        //데이터처리 효율성때문에 BufferedReader를 사용함
-            BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"EUC-KR"));
-            String line = "";
-            
-            while((line = bufReader.readLine()) != null){
-            	// 읽어들인 파일을 split으로 , 별로 잘라서 배열 객체에 저장
-            	String[] strArr = line.split(",");  
-            	// 배열 객체에 저장 되어있는 인덱스별로 하나씩 가져온 후 member객체에 저장
-            	Member member = new Member(strArr[0], strArr[1], strArr[2], strArr[3]);
-            	// member객체에 저장후 각 속성별로 초기화후 members객체에 저장
-            	members.add(new Member(member.getAddress(), member.getIdNumber(), member.getName(), member.getPhoneNumber()));
-            }
-            //.readLine()은 끝에 개행문자를 읽지 않는다.            
-            bufReader.close();
-        	}catch (FileNotFoundException e) {
-            // TODO: handle exception
-        	}catch(IOException e){
-        		System.out.println(e);
-        	}
+			//파일 객체 생성   영화목록
+			File file = new File(files);
+			//데이터처리 효율성때문에 BufferedReader를 사용함
+			BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+			String line = "";
+		            
+			while((line = bufReader.readLine()) != null){
+			// 읽어들인 파일을 split으로 , 별로 잘라서 배열 객체에 저장
+				String[] strArr = line.split(",");
+			// membermanger의 객체안에는 Membranger클래스의 메소드를 가져와서초기화한다. 각속성별로
+				membermanger.sing_up(strArr[0], strArr[1], strArr[2], strArr[3]);
+			}
+			//.readLine()은 끝에 개행문자를 읽지 않는다.            
+			bufReader.close();
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
 	}
 	
-	public static void videoListTrans(String video_list) {
-
-		// 멤버 객체 
-		ArrayList<Video> video_possibility_list = new ArrayList<Video>(); 
-		
-		try{
-            //파일 객체 생성
-	        File file = new File(video_list);
-	        //데이터처리 효율성때문에 BufferedReader를 사용함
-            BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"EUC-KR"));
-            String line = "";
-            
-            while((line = bufReader.readLine()) != null){
-            	// 읽어들인 파일을 split으로 , 별로 잘라서 배열 객체에 저장
-            	String[] strArr = line.split(",");  
-            	// 배열 객체에 저장 되어있는 인덱스별로 하나씩 가져온 후 videos객체에 저장
-            	Video videos = new Video(strArr[0], strArr[1], strArr[2], strArr[3]);
-            	// videos객체에 저장후 각 속성별로 초기화후 videos객체에 저장
-            	video_possibility_list.add(new Video(videos.getGenre(), videos.getMovie_name(), videos.getProduction_year(),videos.getSuperVision()));
-            }
-            //.readLine()은 끝에 개행문자를 읽지 않는다.            
-            bufReader.close();
-        	}catch (FileNotFoundException e) {
-            // TODO: handle exception
-        	}catch(IOException e){
-        		System.out.println(e);
-        	}
-	}
+		//비디오 목 저장  
+		public static void	video_save(String files) {
+			
+			// VideoManger에 담을 객체생성함.
+			VideoManger videomanger = new VideoManger(); 
+				
+			try{
+				//파일 객체 생성   영화목록
+				File file = new File(files);
+				//데이터처리 효율성때문에 BufferedReader를 사용함
+				BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+				String line = "";
+			            
+				while((line = bufReader.readLine()) != null){
+				// 읽어들인 파일을 split으로 , 별로 잘라서 배열 객체에 저장
+					String[] strArr = line.split(",");
+				// video의 객체안에는 video클래스의 메소드를 가져와서초기화한다. 각속성별로
+					videomanger.video_possibility_list.add(new Video(strArr[0],strArr[1],strArr[2],strArr[3]));
+				}
+				//.readLine()은 끝에 개행문자를 읽지 않는다.            
+				bufReader.close();
+			}catch (FileNotFoundException e) {
+				// TODO: handle exception
+			}catch(IOException e){
+				System.out.println(e);
+			}
+			
+		}
 	
 	public static void main(String[] args) {
-			
-			// 회원가입 리스트 저장 함수
-			memberListTrans("C:/Users/nhd59/Desktop/회원목록.txt");
-			
-			// 비디오 리스트 저장 함수 
-			videoListTrans("C:/Users/nhd59/Desktop/비디오목록.txt");
-
-			System.out.print("tsss");
-	}
+		
+		//회원목록 
+		member_save("/Users/choeyeongjae/Desktop/회원목록.txt");
+		
+		//비디오대여목록
+		video_save("/Users/choeyeongjae/Desktop/영화목록.txt");
 		
 	}
-
+}
